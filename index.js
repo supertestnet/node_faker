4d256b879b6a4a50ce1a4b9a22131f98f4c29de8
supertestnet <58400631+supertestnet@users.noreply.github.com>
2025-12-06 19:03:08 +0000

    Update index.js

--- index.js
+++ index.js
@@ -931,7 +931,7 @@ var node_faker = {
                     "localrelay": false,
                     "timeoffset": 0,
                     "networkactive": false,
-                    "connections": 0,
+                    "connections": 5,
                     "networks": [
                         {
                             "name": "ipv4",
